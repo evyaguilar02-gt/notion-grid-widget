dbc89cc005bd612008d47fad5b194833d04b7cc0
evyaguilar02-gt <evy.aguilar02@gmail.com>
2026-05-26 09:44:07 -0600

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,4 +1,4 @@
-xport default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS, GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
